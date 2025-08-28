953e40a89230fbfa5d37a7b448114ab9fe43f84f
gabrielbaute <gabrielbaute@gmail.com>
2025-08-28 13:24:58 -0400

    Snippet para crear directorio instance si no existe

--- landing/database/db_config.py
+++ landing/database/db_config.py
@@ -1,6 +1,12 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 def init_db(app):
+    db_path = os.path.join(os.getcwd(), 'instance')
+    if not os.path.exists(db_path):
+        os.makedirs(db_path)
+    elif not os.access(db_path, os.W_OK):
+        raise PermissionError(f"No se puede escribir en {db_path}")
     db.init_app(app)
