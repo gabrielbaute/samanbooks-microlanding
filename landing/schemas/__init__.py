5b8117361ed87d14efc97233a6dfb7830ffbdd76
gabrielbaute <gabrielbaute@gmail.com>
2025-08-28 10:20:11 -0400

    Módulo de esquemas

---
+++ landing/schemas/__init__.py
@@ -0,0 +1,3 @@
+from landing.schemas.users_schemas import UserCreate, UserResponse, UserUpdate
+from landing.schemas.visits_schemas import VisitsCreate, VisitsResponse
+from landing.schemas.downloads_schemas import DownloadsCreate, DownloadsResponse
